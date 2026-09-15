ddf11903acbfe426a6530ab9683d42b956073135
Tymon <ttime121231@gmail.com>
2026-09-15 18:21:25 -0500

    Add server entry point and health check route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("it works")
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/health", (req, res): void => {
+  res.status(200).json({
+    "success": true,
+    "data": { "status": "ok" },
+    "error": null
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
