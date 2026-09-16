5e1fd1841b1fcde3f8dd32fc0e42df5de72abfaa
Tymon <ttime121231@gmail.com>
2026-09-16 15:14:29 -0500

    Add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./routes/index.js";
+import { logger } from "./middleware/logger.js";
 
 const app = express();
 const port = 3000;
@@ -12,6 +13,8 @@ app.get("/health", (req, res): void => {
   });
 });
 
+app.use(logger);
+app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
