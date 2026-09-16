b1dea229675197d25f98fd7b702bd6d9ffdda1d2
Tymon <ttime121231@gmail.com>
2026-09-16 16:33:45 -0500

    Add consistent error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "./routes/index.js";
 import { logger } from "./middleware/logger.js";
+import { notFoundHandler, errorHandler } from "./middleware/error.js";
 
 const app = express();
 const port = 3000;
@@ -12,10 +13,15 @@ app.get("/health", (req, res): void => {
     "error": null
   });
 });
+app.get('/test-error', (req, res) => {
+  throw new Error('Test error');
+});
 
 app.use(logger);
 app.use(express.json());
 app.use(router);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ src/middleware/error.ts
@@ -0,0 +1,19 @@
+import type { Request, Response, NextFunction } from 'express';
+
+export const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`Route ${req.method} ${req.path} not found`);
+  res.status(404).json({
+    "success": false,
+    "data": null,
+    "error": `Route ${req.method} ${req.path} not found`
+  });
+};
+
+export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(`Error: ${err.message}`);
+  res.status(500).json({
+    "success": false,
+    "data": null,
+    "error": "An error has occurred on the server"
+  });
+};
